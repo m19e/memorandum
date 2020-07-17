c428e3d515bb4013cc1837b8a17da072e1278a7b
m19e <nexus.me7290@gmail.com>
2020-07-18 00:23:37 +0900

    Set props autosave option

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,8 +3,14 @@ import SimpleMDE from "react-simplemde-editor";
 
 const Editor = (): JSX.Element => {
     const [markdown, setMarkdown] = useState("");
+    const opts: EasyMDE.Options = {
+        autosave: {
+            enabled: true,
+            uniqueId: "memorandum",
+        },
+    };
 
-    return <SimpleMDE onChange={(v) => setMarkdown(v)} />;
+    return <SimpleMDE onChange={(v) => setMarkdown(v)} options={opts} />;
 };
 
 export default Editor;
